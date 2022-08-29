dae781eec06465c81c499e63e496db8a714856ed
Diogo Melo <diogobatistavdc1@outlook.com>
2022-08-29 17:36:54 -0300

    Diminuido tamanho da letra, da temperatura atual

--- frontend/src/components/templates/Temperature.jsx
+++ frontend/src/components/templates/Temperature.jsx
@@ -128,7 +128,7 @@ const Temperature = (props) => {
                     <div className="data card d-flex justify-content-center align-items-center mb-3">
 
                         <div className="currentTemp d-flex justify-content-center align-items-center col-12">
-                            <div className="d-flex justify-content-start justify-content-md-center col-5 col-md-6">
+                            <div className="d-flex justify-content-start justify-content-md-end col-5 col-md-6">
                                 <img src={`https://openweathermap.org/img/w/${res.data.weather[0].icon}.png`} alt="Logo" className="icon" />
                             </div>
                             <p className="col-6">{(res.data.main.temp).toFixed(0)}°C</p>
