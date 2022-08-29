8772bc020f017c6f4238a552c1017de52b1c8868
Diogo Melo <diogobatistavdc1@outlook.com>
2022-08-29 17:53:48 -0300

    aaah, responsividade

--- frontend/src/components/templates/Location.jsx
+++ frontend/src/components/templates/Location.jsx
@@ -11,10 +11,22 @@ const Map = (props) => {
             <section className="col-10 col-md-6 col-lg-5 mb-5 mt-5 d-flex justify-content-center align-items-center">
 
                 <div className="mapArea bg-light d-flex justify-content-center col-12 col-md-10">
-                    <div className="sidebar m-3 px-2 py-1"> Latitude: {props.data.mapData.lat} | Longitude: {props.data.mapData.lon} </div>
-                    <img loading="lazy" src={url} alt="Mapa da Cidade" />
+                    <div className="sidebar mt-3 px-2 py-1 d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                        <p className="m-0 me-1">
+                            Latitude: {props.data.mapData.lat}
+                        </p>
+                        <hr className="horizontalLineDivision d-md-none"/>
+                        <span className="m-0 d-none d-md-block">
+                            |
+                        </span>
+                        <p className="m-0 ms-1">
+                            Longitude: {props.data.mapData.lon}
+                        </p>
+                    </div>
+
+                    <img className="col-12" src={url} alt="Mapa da Cidade" />
                 </div>
-            </section>
+            </section >
         )
     }
 }
